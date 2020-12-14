512d9e01de125043d22e01d1a00c17c4a233a664
Gobinath Mallaiyan <gobinathm@21629.no-reply.drupal.org>
2020-12-14 00:21:47 -0500

    - included hook_uninstall  - Converted Token & UserInfo EndPoint as Conditional field  - Update custom validation to work with Conditional field  - Update Endpoint overrides to leverage the new approach of conditional fields

--- src/Plugin/OpenIDConnectClient/AzureB2C.php
+++ src/Plugin/OpenIDConnectClient/AzureB2C.php
@@ -17,6 +17,9 @@ use Drupal\openid_connect\Plugin\OpenIDConnectClientBase;
  */
 class AzureB2C extends OpenIDConnectClientBase {
 
+  // String Used in help texts.
+  const B2C_SAMPLE_URL = "https://login.microsoft.com/<tenant-name>.onmicrosoft.com/<policy-name>/oauth2/v2.0/authorize";
+
   /**
    * Overrides OpenIDConnectClientBase::settingsForm().
    *
@@ -30,22 +33,75 @@ class AzureB2C extends OpenIDConnectClientBase {
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
+    // @todo add sample default endpoint & link to B2C setup Page.
     $form['azure_b2c_authorization_endpoint'] = [
       '#title' => $this->t('Authorization endpoint'),
       '#type' => 'textfield',
-      '#default_value' => $this->configuration['azure_b2c_authorization_endpoint'],
       '#required' => TRUE,
+      '#default_value' => $this->configuration['azure_b2c_authorization_endpoint'] ?? '',
+    ];
+    // @todo . Explain why we say if the token endpoint needs to be overridden.
+    $form['azure_b2c_token_endpoint_override'] = [
+      '#title' => $this->t('Have a custom Token Endpoint ?'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['azure_b2c_token_endpoint_override'] ?? '',
+      '#description' => $this->t('Enabled if you want to provide an overridden Token Endpoint.'),
     ];
     $form['azure_b2c_token_endpoint'] = [
       '#title' => $this->t('Token endpoint'),
       '#type' => 'textfield',
-      '#default_value' => $this->configuration['azure_b2c_token_endpoint'],
+      '#default_value' => $this->configuration['azure_b2c_token_endpoint'] ?? '',
+      '#states' => [
+        'visible' => [
+          ':input[name="clients[azure_b2c][settings][azure_b2c_token_endpoint_override]"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+    // @todo . Explain why UserInfo End Point is not mandatory for B2C.
+    $form['azure_b2c_userinfo_endpoint_exist'] = [
+      '#title' => $this->t('Have a custom UserInfo Endpoint ?'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['azure_b2c_token_endpoint_override'] ?? 0,
+      '#description' => $this->t('Enabled if you have a UserInfo Endpoint to be configured.'),
     ];
     $form['azure_b2c_userinfo_endpoint'] = [
       '#title' => $this->t('UserInfo endpoint'),
       '#type' => 'textfield',
-      '#default_value' => $this->configuration['azure_b2c_userinfo_endpoint'],
+      '#default_value' => $this->configuration['azure_b2c_userinfo_endpoint'] ?? '',
+      '#states' => [
+        'visible' => [
+          ':input[name="clients[azure_b2c][settings][azure_b2c_userinfo_endpoint_exist]"]' => ['checked' => TRUE],
+        ],
+      ],
     ];
+
+    $form['azure_b2c_single_sign_out'] = [
+      '#title' => $this->t('Enable Drupal-invoked single sign-out'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['azure_b2c_sign_out'] ?? 0,
+      '#description' => $this->t("Enabled if Drupal should sign out the user from Azure AD B2C when initiated from a logout is click."),
+    ];
+
+    // Offer to Pass language parameter to AD B2C, on multilingual site.
+    if (\Drupal::languageManager()->isMultilingual()) {
+      $form['azure_b2c_site_language_exchange'] = [
+        '#title' => $this->t('eXchange User Language with Azure AD B2C ?'),
+        '#type' => 'checkbox',
+        '#default_value' => $this->configuration['azure_b2c_sign_out'] ?? 0,
+        '#description' => $this->t("Enabled if Drupal should pass users site language as a parameter to b2c."),
+      ];
+      $form['azure_b2c_site_language_parameter'] = [
+        '#title' => $this->t('Define the Variable name to be used for passing language.'),
+        '#type' => 'textfield',
+        '#default_value' => $this->configuration['azure_b2c_site_language_parameter'] ?? 'language',
+        '#description' => $this->t('By default <b>language</b> would be the Variable passed to B2C, unless overridden above. Ex. URL <a href=":url">:url</a>.', [':url' => self::B2C_SAMPLE_URL]),
+        '#states' => [
+          'visible' => [
+            ':input[name="clients[azure_b2c][settings][azure_b2c_site_language_exchange]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+    }
 
     return $form;
   }
@@ -69,11 +125,14 @@ class AzureB2C extends OpenIDConnectClientBase {
     if (empty($configuration['azure_b2c_authorization_endpoint'])) {
       $form_state->setErrorByName('azure_b2c_authorization_endpoint', $this->t('The Authorization endpoint is missing for @provider.', $provider));
     }
-    if (empty($configuration['azure_b2c_token_endpoint'])) {
+    if ($configuration['azure_b2c_token_endpoint_override'] && empty($configuration['azure_b2c_token_endpoint'])) {
       $form_state->setErrorByName('azure_b2c_token_endpoint', $this->t('The Token endpoint is missing for @provider.', $provider));
     }
-    if (empty($configuration['azure_b2c_userinfo_endpoint'])) {
+    if ($configuration['azure_b2c_userinfo_endpoint_exist'] && empty($configuration['azure_b2c_userinfo_endpoint'])) {
       $form_state->setErrorByName('azure_b2c_userinfo_endpoint', $this->t('The UserInfo endpoint is missing for @provider.', $provider));
+    }
+    if ($configuration['azure_b2c_site_language_exchange'] && empty($configuration['azure_b2c_site_language_parameter'])) {
+      $form_state->setErrorByName('azure_b2c_site_language_parameter', $this->t('The UserInfo endpoint is missing for @provider.', $provider));
     }
   }
 
@@ -85,11 +144,30 @@ class AzureB2C extends OpenIDConnectClientBase {
    *   userinfo object.
    */
   public function getEndpoints() {
-    return [
-      'authorization' => $this->configuration['azure_b2c_authorization_endpoint'],
-      'token' => $this->configuration['azure_b2c_token_endpoint'],
-      'userinfo' => $this->configuration['azure_b2c_userinfo_endpoint'],
-    ];
+    $endpoints = [];
+    $endpoints['authorization'] = $this->generateTokenEndpoints();
+    $endpoints['token'] = $this->generateTokenEndpoints();
+    // Include UserInfo EndPoint if an Override is provided.
+    if ($this->configuration['azure_b2c_userinfo_endpoint_exist'] && !empty($this->configuration['azure_b2c_userinfo_endpoint'])) {
+      $endpoints['userinfo'] = $this->configuration['azure_b2c_userinfo_endpoint'];
+    }
+    return $endpoints;
+  }
+
+  /**
+   * Generates & Returns token endpoint.
+   *
+   * @param string $endpoint_type
+   *   endpoint identifier & defaults to token.
+   *
+   * @return string
+   *   Endpoint URL .
+   */
+  public function generateTokenEndpoints($endpoint_type) {
+    if ($endpoint_type == 'token' && $this->configuration['azure_b2c_token_endpoint_override']) {
+      return $this->configuration['azure_b2c_token_endpoint_override'];
+    }
+    return str_ireplace("/authorize", "/token", $this->configuration['azure_b2c_authorization_endpoint']);
   }
 
 }
